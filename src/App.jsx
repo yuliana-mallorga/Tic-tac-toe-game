64387d9b5275788815510bfda3dcb083d13307bc
yulianaM <yulianamallorga@gmail.com>
2023-09-13 12:33:55 -0300

    Create the Square component to be rendered in the App, showing on the board according to the turns of each player X or O.

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import "./App.css";
+
+const TURNS = {
+  X: "X",
+  O: "O",
+};
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const classNameSquare = `square ${isSelected ? 'is-selected' : ''}`
+  
+  const handleClick = () =>{
+    updateBoard(index)
+  }
+  return (
+    <div onClick={handleClick} className={classNameSquare}> 
+       {children}
+    </div>
+    )
+};
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X);
+
+  const updateBoard = (index) => {
+    const newBoard = [... board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+    console.log("updateB", newBoard[index], newBoard, 'turno que va', turn );
+
+  }
+  return (
+    <>
+      <main className="board">
+        <h1>Tic tac toe</h1>
+        <section className="game">
+          {board.map((_, index) => {
+            return (
+              <Square 
+              key={index} 
+              index={index}
+              updateBoard={updateBoard}
+              >
+               {board[index]}
+              </Square>
+            );
+          })}
+        </section>
+        <section className="turn">
+          <Square isSelected={turn === TURNS.X}>
+            {TURNS.X} 
+          </Square>
+          <Square isSelected={turn === TURNS.O}>
+            {TURNS.O} 
+          </Square>
+        </section> 
+      </main>
+    </>
+  );
+}
+
+export default App;
